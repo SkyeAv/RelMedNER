8c9c48e7dc3f63a64c6af36507bababb6b63e70b
SkyeAv <skye.a.goetz@gmail.com>
2026-09-15 16:02:17 -0700

    chore: updated ingests modeling to work with one real use case

--- src/relmedner/models.py
+++ src/relmedner/models.py
@@ -9,12 +9,15 @@ from dataclasses_avroschema.pydantic import AvroBaseModel
 class StrictBase(AvroBaseModel):
     model_config: ConfigDict = ConfigDict(frozen=True, extra="forbid")
 
-
 class YamlIngests(StrictBase):
     datasets: list[Dataset] = Field(...)
 
-
 class Dataset(StrictBase):
-    type: Literal["hf"] = Field(...)
-    name: str = Field(...)
-    splits: list[str] = Field(...)
+    type: Literal["vocab"] = Field(...)
+
+class HuggingFaceDataset(Dataset)
+    source: Literal["hf"] = Field(...)
+    dataset: str = Field(...)
+    subset: str = Field(...)
+    split: str = Field(...)
+    column: str = Field(...)
