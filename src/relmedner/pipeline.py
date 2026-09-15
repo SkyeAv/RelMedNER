e475ba447c71c6ca9f018a5a744e8f9927d71c16
SkyeAv <skye.a.goetz@gmail.com>
2026-09-15 14:47:00 -0700

    feat: converted build_pipeline to a class

--- src/relmedner/pipeline.py
+++ src/relmedner/pipeline.py
@@ -1,9 +1,14 @@
 import apache_beam as beam
 
+from typing import Self
 
-def build_pipeline() -> None:
-    with beam.Pipeline as new_pipeline:
-        (
-            new_pipeline
-            | "" >> beam.Create()
-        )
+class BuildPipeline:
+    def __init__(self: Self) -> None:
+        return None
+
+    def run() -> None:
+        with beam.Pipeline as new_pipeline:
+            (
+                new_pipeline
+                | "" >> beam.Create()
+            )
