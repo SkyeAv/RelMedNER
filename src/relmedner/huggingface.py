13de3f3ee1b7d11c33651bebf8ae2eb3d3d8928a
SkyeAv <skye.a.goetz@gmail.com>
2026-09-16 15:43:52 -0700

    feat: made the hugging face datastream tuple-native

--- src/relmedner/huggingface.py
+++ src/relmedner/huggingface.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Self, Optional, Union
+from typing import Self, Optional, Any
 from collections.abc import Iterator
 
 from datasets import load_dataset
@@ -13,18 +13,18 @@ class HuggingFaceDataStream:
         dataset: str,
         subset: Optional[str],
         split: Optional[str],
-        match_on: Optional[list[dict[str, Union[str, list[str]]]]],
-        columns_out: list[str],
+        match_on: Optional[tuple[tuple[str, tuple[str, ...]], ...]],
+        columns_out: tuple[str, ...],
     ) -> None:
         self.type: str = type
         self.dataset: str = dataset
         self.subset: Optional[str] = subset
         self.split: Optional[str] = split
-        self.match_on: Optional[list[dict[str, Union[str, list[str]]]]] = match_on
-        self.columns_out: list[str] = columns_out
+        self.match_on: tuple[tuple[str, frozenset[str]], ...] = tuple((column, frozenset(values)) for column, values in match_on) if match_on else ()
+        self.columns_out: tuple[str, ...] = columns_out
 
-    def apply_match(self: Self, row) -> bool:
-        return any(any(transform.get("values") in row.get(column) for column in transform.get("column")) for transform in self.match_on)
+    def apply_match(self: Self, row: dict[str, Any]) -> bool:
+        return all(row.get(column) in values for column, values in self.match_on)
 
     def generate_rows(self: Self) -> Iterator[tuple[str, tuple[Optional[str], ...]]]:
         datastream = load_dataset(self.dataset, self.subset, split=self.split, streaming=True)
