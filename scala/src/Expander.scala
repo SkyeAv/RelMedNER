2d3c80b3a81ed3ceecca8354861f8bb63ba13cba
SkyeAv <skye.a.goetz@gmail.com>
2026-09-16 10:34:11 -0700

    chore: made scala code more minimal

--- scala/src/Expander.scala
+++ scala/src/Expander.scala
@@ -1,13 +1,8 @@
 package relmedner
-import scalatags.Text.all.*
 
 object Expander {
-  def generateHtml(text: String) = {
-    h1(text).toString
-  }
-
   @main
   def main() = {
-    println(generateHtml("hi"))
+    println("Hello World")
   }
 }
